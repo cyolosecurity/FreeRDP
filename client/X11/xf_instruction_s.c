4993367bb711c4b47d4824d6f8e7f8b61d1e4f28
yellin <jonathan@cyolo.io>
2024-05-21 17:45:58 +0300

    'enum MHD_Result' is not compiling in the docker environmnet, so let's just use int

--- client/X11/xf_instruction_s.c
+++ client/X11/xf_instruction_s.c
@@ -41,7 +41,7 @@ static int send_response(struct MHD_Connection* connection, unsigned int status_
 	return ret;
 }
 
-static enum MHD_Result answer_to_connection(void* cls, struct MHD_Connection* connection, const char* url,
+static int answer_to_connection(void* cls, struct MHD_Connection* connection, const char* url,
                                 const char* method, const char* version, const char* upload_data,
                                 size_t* upload_data_size, void** con_cls)
 {
